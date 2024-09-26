009dd06495ffbf2af33f076689bfb529d82743ed
rylan@trackd.run <rylan@trackd.run>
2024-09-26 16:23:11 -0500

    finish starwars, started absVal

--- starwar.py
+++ starwar.py
@@ -1,1 +1,10 @@
-name = input()
+input()
+a = int(input())
+b = int(input())
+c = int(input())
+if a - b > 0 and c > 0:
+    print("VEIT EKKI")
+elif a - b != c:
+    print("SITH")
+else:
+    print("JEDI")
