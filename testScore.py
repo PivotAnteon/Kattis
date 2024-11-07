435343b6196b44e24385e45ea2feb30c4d3d9740
rmelch <rylan@trackd.run>
2024-11-07 16:24:26 -0600

    In progress Kattis

--- testScore.py
+++ testScore.py
@@ -4,11 +4,11 @@ results = []
 take = 47 
 while (take != -1) :
     take = input()
-    if take == -1:
+    if int(take) == -1:
         break
     t = int(take[0])
-    que = str(take[1])
-    result = str(take[2])
+    que = take[1]
+    result = take[2]
     
     ts.append(t)
     ques.append(que)
