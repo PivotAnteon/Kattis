b7490214ca64df8140f517acb1014ae7a3856d39
rmelch <rylan@trackd.run>
2024-11-13 16:42:36 -0600

    Finished ACM Contest Scoring Kattis Problem

--- testScore.py
+++ testScore.py
@@ -1,14 +1,18 @@
 ts = []
 ques = []
 results = []
-take = 47 
-while (take != -1) :
+
+while (True) :
     take = input()
-    if int(take) == -1:
+    
+    if take == '-1':
         break
-    t = int(take[0])
-    que = take[1]
-    result = take[2]
+    
+    parts = take.split()
+    
+    t = int(parts[0])
+    que = parts[1]
+    result = parts[2]
     
     ts.append(t)
     ques.append(que)
@@ -16,4 +20,20 @@ while (take != -1) :
     
 a = list(zip(ts, ques, results))
 
-print(a)
+numQScore = 0
+timeScore = 0
+penalty = 0
+for i in a:
+    if i[2] == 'right':
+        numQScore += 1
+        timeScore += i[0]
+        for j in a:
+            if j[1] == i[1] and j[0] <= i[0] and j[2] != i[2]:
+                penalty += 1
+
+
+penalty = penalty * 20
+
+totalScore = timeScore + penalty
+
+print(str(numQScore) + " " + str(totalScore))
